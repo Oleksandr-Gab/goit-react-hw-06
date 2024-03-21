6630ab106b43003b68d973bbf290ab8ff3f5fb6a
Olexsandr <Saniafan1@gmail.com>
2024-03-22 00:08:37 +0200

    start

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,23 @@
+import { useSelector } from "react-redux";
+
+import { selectContacts } from "../../redux/contactsSlice.js";
+import { selectNameFilter } from "../../redux/filtersSlice.js";
+
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 
-export default function ContactList({ visibleContacs, onDelete }) {
+export default function ContactList() {
+    const contacts = useSelector(selectContacts);
+    const filterValue = useSelector(selectNameFilter);
+
+    const contactsToRender = contacts.filter((contact) =>
+        contact.name.includes(filterValue.trim())
+    );
     return (
         <ul className={css.contactList}>
-            {visibleContacs.map((contact) => (
+            {contactsToRender.map((contact) => (
                 <li key={contact.id}>
-                    <Contact contact={contact} onDelete={onDelete} />
+                    <Contact contact={contact} />
                 </li>
             ))}
         </ul>
